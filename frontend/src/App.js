81c2a1440fe43908bb38eab110151ff848b066c7
nirasha9806 <nirasha.chamudini@gmail.com>
2021-08-21 03:09:17 +0530

    Add and view reservation frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import AddReservation from "./components/nirasha/reservation-component/addReservation";
+import ViewReservations from "./components/nirasha/reservation-component/viewResrvations";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-           React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <section>
+          <Switch>
+            <Route path="/addReservation" component={AddReservation} />
+            <Route path="/viewReservation" component={ViewReservations} />
+          </Switch>
+        </section>
+      </Router>
     </div>
   );
 }
